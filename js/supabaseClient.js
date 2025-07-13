cf1d36ffff9336bca961d3b184269e7ac048ef58
jiang <2831392974@qq.com>
2025-07-13 15:42:05 +0800

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // 配置Supabase（替换为您的实际信息）
-const supabaseUrl = 'https://sncryqjgutdauefdwsqf.supabase.co'; // 替换为第一步获取的URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuY3J5cWpndXRkYXVlZmR3c3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMjMwMzgsImV4cCI6MjA2Nzg5OTAzOH0.9oLODVtw3hNUtMG-HzwQytRfmtrB-xWSsgrh3rFrXKA'; // 替换为第一步获取的密钥
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // 初始化客户端
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
